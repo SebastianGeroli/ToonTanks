d7ddc42ae71899ecffe177d645f692354d316856
Sebastian Geroli <sgeroli@widowgames.com>
2021-11-29 21:43:54 -0300

    Forward declaration capsule component

--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -18,12 +18,14 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
+private:
+	UPROPERTY()
+	class UCapsuleComponent* CapsuleComponent;
+	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 };
