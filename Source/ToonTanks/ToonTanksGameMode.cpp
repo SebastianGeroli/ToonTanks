9c2d4fef4c374268d32ca56c6754539f543c51a6
Sebastian Geroli <sebastiangeroli@outlook.com>
2021-12-03 22:51:12 -0300

    Game Over Widget

--- Source/ToonTanks/ToonTanksGameMode.cpp
+++ Source/ToonTanks/ToonTanksGameMode.cpp
@@ -17,10 +17,16 @@ void AToonTanksGameMode::ActorDied(AActor* DeadActor)
 		{
 			ToonTanksPlayerController->SetPlayerEnabledState(false);
 		}
+		GameOver(false);
 	}
 	else if (ATower* DestroyedTower = Cast<ATower>(DeadActor))
 	{
 		DestroyedTower->HandleDestruction();
+		TargetTowers--;
+		if (TargetTowers <= 0)
+		{
+			GameOver(true);
+		}
 	}
 }
 
@@ -32,6 +38,7 @@ void AToonTanksGameMode::BeginPlay()
 
 void AToonTanksGameMode::HandleGameStart()
 {
+	TargetTowers = GetTargetTowerCount();
 	Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
 	ToonTanksPlayerController = Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this, 0));
 	StartGame();
@@ -45,4 +52,11 @@ void AToonTanksGameMode::HandleGameStart()
 			true);
 		GetWorldTimerManager().SetTimer(PlayerEnabledTimerHandle, PlayerEnableTimerDelegate, StartDelay, false);
 	}
+}
+
+int32 AToonTanksGameMode::GetTargetTowerCount()
+{
+	TArray<AActor*> Towers;
+	UGameplayStatics::GetAllActorsOfClass(this, ATower::StaticClass(), Towers);
+	return Towers.Num();
 }
